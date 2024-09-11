e5273034fa6945ecb28c5aa257f22c7394d93050
Lou Bellaire <r.louis.bellaire@ll.mit.edu>
2024-09-11 10:08:22 -0400

    Update linestring.ts
    Additional change to comply with the APACHE license terms

--- src/annotation/linestring.ts
+++ src/annotation/linestring.ts
@@ -1,4 +1,9 @@
 /**
+ * Copyright (C) 2024, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+ * Subject to FAR 52.227-11 – Patent Rights – Ownership by the
+ * Contractor (May 2014) 
+ * SPDX-License-Identifier: BSD-2-Clause
+ * The software/firmware is provided to you on an As-Is basis
  * @file Support for rendering line string annotations.
  */
 
